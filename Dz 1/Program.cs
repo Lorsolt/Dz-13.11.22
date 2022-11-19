30b4ae7fbe1ad1a68d50e70463479f44d74018c0
Lorsolt <115705447+Lorsolt@users.noreply.github.com>
2022-11-20 00:26:43 +0300

    Update Program.cs

--- Dz 1/Program.cs
+++ Dz 1/Program.cs
@@ -4,20 +4,6 @@
 // 782 -> 8
 // 918 -> 1
 
-// Console.WriteLine("Введите число: ");
-// string number = (Console.ReadLine());
-// int num = number!.Length;
-// if (num == 3)
-// {
-//     Console.WriteLine($"Второе Число: {number[1]} ");
-// }
-// else
-// {
-//     Console.WriteLine($"Тебя просят ввести трехзначное число, fratello");
-// }
-
-
-
 
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
